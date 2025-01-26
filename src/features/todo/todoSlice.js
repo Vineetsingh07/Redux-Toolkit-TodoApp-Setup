7bee3a8a8030c29704c34a0b30997b7d825371bb
Vineetsingh07 <vineetsingh2603@gmail.com>
2025-01-27 00:34:21 +0530

    Add Edit Todo method in reducers

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -5,9 +5,25 @@ const initialState = {
 };
 
 export const todoSlice = createSlice({
-  name:"todo",
+  name: "todo",
   initialState,
-  reducers:{
-    
-  }
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = todos.filter((todo) => todo.id !== action.payload);
+    },
+    editTodo: (state, action) => {
+      const { id, text } = action.payload;
+
+      state.todos = state.todos.map((todo) =>
+        todo.id === id ? { ...todo, text: text } : todo
+      );
+    },
+  },
 });
